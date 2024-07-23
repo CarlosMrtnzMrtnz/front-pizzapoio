bf4f52712d46dbd1c8d16ba8018d54a7ae3463e6
CarlosMrtnzMrtnz <carlosmartinezmartinez2015@gmail.com>
2024-07-23 13:36:21 -0500

    cambio de ip de services

--- src/app/services/pizza-poio.service.ts
+++ src/app/services/pizza-poio.service.ts
@@ -7,7 +7,7 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class PizzaPoioService {
 private http = inject(HttpClient)
-private urlApi: string  ='http://localhost:4000/api'
+private urlApi: string  ='http://35.173.242.54:4000/api'
 
 constructor() { }
 
